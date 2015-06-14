dd5d24c59a7a1ce4d0a8b1cb4fb8e71438d14a8a
Teodor Valov <Ted@Teodors-MacBook.local>
2015-06-14 14:47:49 +0300

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tvspoilerbgApp
+ * @description
+ * # tvspoilerbgApp
+ *
+ * Main module of the application.
+ */
+angular.module('tvspoilerbgApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
